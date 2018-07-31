31baea57329289f2f1c2696fa1a9ca0e5bfec32c
fealty <421080328@qq.com>
2018-07-31 09:28:24 +0800

    使用hibernate特性完成按照父节点innercode查找所有的子节点

--- src/main/java/com/hit/cost/controller/OrgController.java
+++ src/main/java/com/hit/cost/controller/OrgController.java
@@ -3,11 +3,18 @@ package com.hit.cost.controller;
 import com.hit.cost.bean.Org;
 import com.hit.cost.jpa.OrgJPA;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * =========================
@@ -32,4 +39,27 @@ public class OrgController {
     public  List<Org> queryAllBusinessUnit(){
         return orgJPA.findAllBusinessUnit();
     }
+    @RequestMapping(value = "code",method = RequestMethod.POST)
+    public Org findbyCode(@RequestParam(name="code") String code){
+        Optional<Org> res= orgJPA.findOne(new Specification<Org>() {
+            @Override
+            public Predicate toPredicate(Root<Org> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                query.where(criteriaBuilder.equal(root.get("code"),code));
+                return null;
+            }
+        });
+        return  res.get();
+    }
+    @RequestMapping(value = "sons",method = RequestMethod.POST)
+    public List<Org> findSons(@RequestParam(name="innercode") String innercode){
+       return orgJPA.findAll(new Specification<Org>() {
+            @Override
+            public Predicate toPredicate(Root<Org> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                query.where(criteriaBuilder.like(root.get("innercode"),innercode+"%"));
+                return null;
+            }
+        });
+    }
+
+
 }
